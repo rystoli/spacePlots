886a55de27247ffc572acf0f995cf3a5040fb1aa
Ryan M. Stolier <ryan.m.stolier@gmail.com>
2018-02-18 22:40:02 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='spaceplots',
+        version='0.0.1',
+        description='Distance matrix summary plots',
+        author='Ryan Stolier',
+        author_email='rystoli@nyu.edu',
+        url='https://github.com/rystoli/spacePlots',
+        packages=['spacePlots']
+        )
